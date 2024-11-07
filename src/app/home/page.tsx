c88161d31ed5926620018dba36e8e4282c475301
Mary Nguyen <mpn64894@uga.edu>
2024-11-07 11:19:49 -0500

    marysBranch

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,6 +12,7 @@ export default function HomePage() {
         <div>
             <Nav/>
             <h1>{formattedDate}</h1>
+            
             <TodayPlan/>
             <JournalEntry />
         </div>
